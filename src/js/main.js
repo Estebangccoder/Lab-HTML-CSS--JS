8f27a326173aaef8943f51aff9a945d014e0c0da
estebangccoder <estebangccoder@gmail.com>
2025-07-18 19:28:48 -0500

    The workshop is finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,20 @@
+// LABORATORIO HTML, CSS Y JS
+// 1. Crea una página que:
+// 1.
+// Muestre un contador, los estilos dependen de cada uno.
+// 2.
+// Hacer un botón para incrementar el contador (Con JS).
+// 2. Agregar un nuevo botón, que ahora, decrementa el conteo.
+// 3. Cambie el color del contador cuando llegue a 10.
+
+
+
 const body= document.querySelector("body")
 
 let contador=0
+
+
+
 function mostrarHTML(inicial){body.innerHTML=`
  <header>
         <h1>Contador</h1>
@@ -17,6 +31,8 @@ function mostrarHTML(inicial){body.innerHTML=`
     
 `;
 
+const numero =document.querySelector("#numero")
+
 document.querySelector("#boton_aumentar").addEventListener('click', () => {
     contador++
     mostrarHTML(contador)
@@ -26,7 +42,15 @@ document.querySelector("#boton_disminuir").addEventListener('click', () => {
     contador--
     mostrarHTML(contador)
 });
+cambiarContador();
+}
 
+function cambiarContador(){
+    if (contador>9){
+        numero.style.color="red";
+    }else{
+        numero.style.color="black";
+    };
 }
 
 
